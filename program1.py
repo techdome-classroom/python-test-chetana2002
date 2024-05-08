225d7d283bf5a98ea99afd45d7e35dec9c06377b
chetana dharavathu <chichu2901@gmail.com>
2024-05-08 11:06:03 +0530

    Done

--- program1.py
+++ program1.py
@@ -1,6 +1,36 @@
 class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
+        if not grid:
+            return 0
+
+        count = 0
+        # Iterate through each cell in the grid
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                # If the current cell contains a landmass, perform a depth-first search to mark all connected landmasses
+                if grid[i][j] == 'L':
+                    # Increment the counter for the number of islands
+                    self.dfs(grid, i, j)
+                    count += 1
+        
+        
+        # Return the number of distinct islands
+        return count
+
+    def dfs(self, grid, i, j):
+        # Define the boundaries of the grid
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j]!='L':
+            return
+        
+        # Mark the current cell as visited by changing it to '#'
+        grid[i][j] = '#'
+        # Recursively visit neighboring cells that are also landmasses
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
+
     #    write your code here
                     
         return 0
--- program2.py
+++ program2.py
@@ -1,5 +1,33 @@
-def decode_message( s: str, p: str) -> bool:
+def decode_message(s: str, p: str) -> bool:
+# Initialize two pointers, one for the secret message and one for the pattern
+    s_index = 0
+    p_index = 0
+
+
+    #Iterate through the pattern
+    while p_index < len(p):
+        #If the current character in the pattern is a star symbol, skip over it
+        if p[p_index] == '*':
+                # skip over consecutive star symbols
+            while p_index < len(p) and p[p_index] == '*':
+                p_index += 1
+            #If the pattern has been fully processed, then return True    
+            if p_index == len(p):
+                return True
+            #Skip over characters in the secret message until a match is found   
+            while s_index < len(s) and (p_index == len(p) or s[s_index]!= p[p_index]):
+                s_index += 1
+        # If the current character in the pattern is a question mark or matches the current character in the secret message, move to the next character        
+        elif p[p_index] == '?' or p[p_index] == s[s_index]:
+            s_index += 1
+            p_index += 1
+         # If the characters do not match, return False    
+        else:
+            return False
+
+    # If the entire secret message has been processed, return True
+    return s_index == len(s)
+        
 
 # write your code here
   
-        return False
